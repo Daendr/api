0195357f62696c97a702cda733b89bd4d4dd87a3
Daendr <daendr@gmail.com>
2023-12-25 11:56:12 +0300

    Правки выполнены

--- apiUtils/apiutils.py
+++ apiUtils/apiutils.py
@@ -1,39 +1,47 @@
 import json
-
+import os
 import requests
+from models.user import User
 from utilities.configManager import ConfigManager
-from utilities.jsonlogger import JsonLogger
 
 
 class ApiUtils:
 
     @staticmethod
-    def rest_post(url_parts, data):
+    def r_post(url_parts, data):
         base_url = ConfigManager.get_config_value("Url")
         response = requests.post(base_url+url_parts, data)
 
         return response
 
     @staticmethod
-    def rest_get(url_parts):
+    def r_get(url_parts):
         base_url = ConfigManager.get_config_value("Url")
         response = requests.get(base_url+url_parts)
-
         return response
 
     @staticmethod
-    def content_type(response):
-        try:
+    def get_content_type(response):
             response.raise_for_status()
             content_type = response.headers.get('content-type')
-            if 'application/json' not in content_type:
-                return False
-            return True
-        except requests.exceptions.RequestException as e:
-            raise SystemExit(e)
+            return 'application/json' not in content_type
 
     @staticmethod
-    def id_sort(response):
+    def is_sort_by_value(response, value):
         posts = json.loads(response.text)
-        ids = [post["id"] for post in posts]
-        return ids == sorted(ids)
+        posts_list = [post[value] for post in posts]
+        return posts_list == sorted(posts_list)
+
+    @staticmethod
+    def give_expected_data():
+        json_path = os.path.join('..', 'test_data', 'user_data.json')
+        with open(json_path, "r", encoding="utf-8") as file:
+            expected_user_data_dict = json.load(file)
+        expected_user_data = User.from_dict(expected_user_data_dict)
+        return expected_user_data
+
+    @staticmethod
+    def get_value(responce, value, content_type):
+        users_list = json.loads(responce.text)
+        user = next((user for user in users_list if user.get(content_type) == value), None)
+        return User.from_dict(user)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,4 @@
 import json
-import os
-
 import pytest
 import requests
 from py_selenium_auto_core.logging.logger import Logger
@@ -22,13 +20,14 @@ class TestApi:
     @pytest.mark.test_api
     def test_api(self):
         Logger.info("Шаг 1. Отправьте запрос GET, (/posts).")
-        responce_from_url = ApiUtils.rest_get(ConfigManager.get_config_value("Posts"))
+        responce_from_url = ApiUtils.r_get(ConfigManager.get_config_value("Posts"))
         assert responce_from_url.status_code == requests.codes.ok, "Статус код не равен 200"
-        assert ApiUtils.content_type(responce_from_url), "Тело ответа не JSON"
-        assert ApiUtils.id_sort(responce_from_url), "Сообщения не упорядочены по возрастанию"
+        assert ApiUtils.get_content_type(responce_from_url), "Тело ответа не JSON"
+        assert ApiUtils.is_sort_by_value(responce_from_url, ConfigManager.get_config_value("Value")),\
+            "Сообщения не упорядочены по возрастанию"
 
         Logger.info("Шаг 2. Отправьте запрос GET, с id=99")
-        responce_from_url = ApiUtils.rest_get(ConfigManager.get_config_value("ParamPost").format(str(self._id_step_2)))
+        responce_from_url = ApiUtils.r_get(ConfigManager.get_config_value("ParamPost").format(str(self._id_step_2)))
         assert responce_from_url.status_code == requests.codes.ok, "Статус код не равен 200"
         assert Root.from_dict(json.loads(responce_from_url.text)).id == self._id_step_2, "id not equals"
         assert Root.from_dict(json.loads(responce_from_url.text)).userId == self._expected_userId, "userId not equals"
@@ -36,7 +35,7 @@ class TestApi:
         assert Root.from_dict(json.loads(responce_from_url.text)).body, "Тело не должно быть пустым"
 
         Logger.info("Шаг 3. Отправьте запрос GET, с id=150.")
-        responce_from_url = ApiUtils.rest_get(ConfigManager.get_config_value("ParamPost").format(str(self._id_step_3)))
+        responce_from_url = ApiUtils.r_get(ConfigManager.get_config_value("ParamPost").format(str(self._id_step_3)))
         assert responce_from_url.status_code == requests.codes.not_found, "Статус код не равен 404"
         assert responce_from_url.text == ConfigManager.get_config_value("EmptyContent"),\
             "Тело ответа не должно содержать данных при статусе 404"
@@ -45,37 +44,26 @@ class TestApi:
         post_data = {
             'title': GenerateRandomText.generate_random_text(),
             'body': GenerateRandomText.generate_random_text(),
-            'userId': self._id_step_4
-        }
-        responce_from_url = ApiUtils.rest_post(ConfigManager.get_config_value("Posts"), post_data)
+            'userId': self._id_step_4}
+        responce_from_url = ApiUtils.r_post(ConfigManager.get_config_value("Posts"), post_data)
         assert responce_from_url.status_code == requests.codes.created, "Статус код не равен 201"
+        assert Root.from_dict(json.loads(responce_from_url.text)).title == post_data['title'],\
+            "Заголовок не соответствует ожидаемому"
+        assert Root.from_dict(json.loads(responce_from_url.text)).body == post_data['body'],\
+            "Тело не соответствует ожидаемому"
         created_post = json.loads(responce_from_url.text)
-        assert Root.from_dict(json.loads(responce_from_url.text)).title == post_data['title'], "Заголовок не соответствует ожидаемому"
-        assert Root.from_dict(json.loads(responce_from_url.text)).body, "Тело не соответствует ожидаемому"
         assert created_post['userId'] == str(post_data['userId']), "userId не соответствует ожидаемому"
         assert 'id' in created_post, "Отсутствует 'id' в ответе"
 
-        Logger.info("Шаг 5. Отправьте запрос GET, чтобы получить пользователя.")
-        responce_from_url = ApiUtils.rest_get(ConfigManager.get_config_value("Users"))
-        assert ApiUtils.content_type(responce_from_url), "Тело ответа не JSON"
+        Logger.info("Шаг 5. Отправьте запрос GET, и получите заданного пользователя.")
+        responce_from_url = ApiUtils.r_get(ConfigManager.get_config_value("Users"))
+        assert ApiUtils.get_content_type(responce_from_url), "Тело ответа не JSON"
         assert responce_from_url.status_code == requests.codes.ok, "Статус код не равен 200"
-        users_list = json.loads(responce_from_url.text)
-        user_with_id_5 = next((user for user in users_list if user.get("id") == self._id_step_5), None)
-        assert user_with_id_5, f"Пользователь с id={self._id_step_5} не найден в списке пользователей"
-        json_path = os.path.join('..', 'test_data', 'user_data.json')
-        with open(json_path, "r", encoding="utf-8") as file:
-            expected_user_data_dict = json.load(file)
-        expected_user_data = User.from_dict(expected_user_data_dict)
-        actual_user_data = User.from_dict(user_with_id_5)
-        assert actual_user_data == expected_user_data, "Данные пользователя не соответствуют ожидаемым"
+        assert (ApiUtils.get_value(responce_from_url, self._id_step_5, ConfigManager.get_config_value("Value"))
+                == ApiUtils.give_expected_data()), "Данные пользователя не соответствуют ожидаемым"
 
         Logger.info("Шаг 6. Отправьте запрос GET,  с id=5 (/users/5).")
-        responce_from_url = ApiUtils.rest_get(ConfigManager.get_config_value("ParamUsers").format(str(self._id_step_5)))
+        responce_from_url = ApiUtils.r_get(ConfigManager.get_config_value("ParamUsers").format(str(self._id_step_5)))
         assert responce_from_url.status_code == requests.codes.ok, "Статус код не равен 200"
-        user_with_id_5 = json.loads(responce_from_url.text)
-        json_path = os.path.join('..', 'test_data', 'user_data.json')
-        with open(json_path, "r", encoding="utf-8") as file:
-            expected_user_data_dict = json.load(file)
-        expected_user_data = User.from_dict(expected_user_data_dict)
-        actual_user_data = User.from_dict(user_with_id_5)
-        assert actual_user_data == expected_user_data, "Данные пользователя не соответствуют ожидаемым"
+        assert User.from_dict(json.loads(responce_from_url.text)) == ApiUtils.give_expected_data(),\
+            "Данные пользователя не соответствуют ожидаемым"
